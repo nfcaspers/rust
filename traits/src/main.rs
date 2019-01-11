90ca9f51a056de9181092c2c87432839a11d8c83
nfcaspers <noelcaspers04@gmail.com>
2019-01-11 23:15:16 +0100

    traits now working

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,1 +1,54 @@
-fn main() {}
+#![allow(dead_code)]
+fn main() {
+    let mark = Person {name: String::from("Mark"), age: 20};
+    let ray = Dog {name: String::from("Ray"), age: 5, breed: DogBreed::GoldenRetriver};
+
+    println!("Info about Mark: {}", mark.info());
+    println!("Info about Ray: {}", ray.info());
+}
+
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl Info for Person {
+    fn info(&self) -> String {
+        String::from(format!("Name: {}, Age: {}", self.name, self.age))
+    }
+}
+
+struct Dog {
+    name: String,
+    age: u8,
+    breed: DogBreed 
+}
+
+impl Info for Dog {
+    fn info(&self) -> String {
+        String::from(format!("Name: {}, Age: {}, Breed: {}", 
+                            self.name, self.age,
+                            self.breed.breed_name_as_str()))
+    }
+}
+
+enum DogBreed {
+    GoldenRetriver,
+    GermanShepherd,
+    GreyHound,
+}
+
+impl DogBreed {
+    fn breed_name_as_str(&self) -> &str {
+        match self {
+            DogBreed::GoldenRetriver => "Golden Retriver",
+            DogBreed::GermanShepherd => "German Shepherd",
+            DogBreed::GreyHound => "Greyhound",
+        }
+    }
+}
+
+//format string with info about something
+trait Info {
+    fn info(&self) -> String;
+}
