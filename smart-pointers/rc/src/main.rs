0c3509d04e26f6536e2acf29b023032ac7252d40
nfcaspers <noelcaspers04@gmail.com>
2019-02-16 12:49:27 +0100

    worked on rc-smartpointer usage

--- smart-pointers/rc/src/main.rs
+++ smart-pointers/rc/src/main.rs
@@ -1,3 +1,37 @@
 use std::rc::{Rc, Weak};
 
-fn main() {}
+fn main() {
+    let owner = Owner::new();
+    let weakref = WeakRef::new(&owner);
+
+    drop(owner);
+    //if assert is true the Value has been dropped
+    assert!(weakref._ref.upgrade().is_none());
+
+}
+
+#[derive(Debug)]
+struct Owner {
+    pub owned: Rc<i32>,
+}
+
+impl Owner {
+    fn new() -> Owner {
+        Owner {
+            owned: Rc::new(10),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct WeakRef {
+    pub _ref: Weak<i32>,
+}
+
+impl WeakRef {
+    fn new(value_owner: &Owner) -> WeakRef {
+        WeakRef {
+            _ref: Rc::downgrade(&value_owner.owned)
+        }
+    }
+}
