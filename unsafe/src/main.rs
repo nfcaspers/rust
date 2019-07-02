b7517dbd18a6e9a6cdcc465321426862dc799e7a
nfcaspers <noelcaspers04@gmail.com>
2019-07-02 22:51:05 +0200

    working with unions

--- unsafe/src/main.rs
+++ unsafe/src/main.rs
@@ -1,7 +1,25 @@
+#![allow(dead_code)]
+
+#[derive(Clone, Copy)]
+struct ErrInfo;
+
+union ErrUnion {
+    recover: ErrInfo, 
+    fail: u8,
+}
+
 fn main() {
+    //cast ref to pointer and deref it
     let x: i64 = 7;
     unsafe {
         let ptr = &x as *const i64;
-        println!("{:?}", *ptr);
+        println!("Derefed pointer: {:?}", *ptr);
+    }
+
+    //use union
+    let e = ErrUnion{ fail: 100 };
+    unsafe {
+        let err_val = e.fail;
+        println!("Value of Error is: {}", err_val)
     }
 }
